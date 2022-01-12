7ab49218fe9a2b5a94d03d875626e46ce2a88919
Lumi Miettinen <luminaula@gmail.com>
2022-01-12 21:41:27 +0200

    register macros

--- inc/register.h
+++ inc/register.h
@@ -12,266 +12,6 @@
 #include <stdatomic.h>
 #include <stdint.h>
 
-#define ASMTF_THREAD_STORE_RDI(loc) \
-    __asm__(                        \
-        "mov %0, rdi\n\t"           \
-        :                           \
-        : "m"(loc)                  \
-        :)
-#define ASMTF_THREAD_STORE_RSI(loc) \
-    __asm__(                        \
-        "mov %0, rsi\n\t"           \
-        :                           \
-        : "m"(loc)                  \
-        :)
-#define ASMTF_THREAD_STORE_RDX(loc) \
-    __asm__(                        \
-        "mov %0, rdx\n\t"           \
-        :                           \
-        : "m"(loc)                  \
-        :)
-#define ASMTF_THREAD_STORE_RCX(loc) \
-    __asm__(                        \
-        "mov %0, rcx\n\t"           \
-        :                           \
-        : "m"(loc)                  \
-        :)
-
-#define ASMTF_THREAD_STORE_RBX(loc) \
-    __asm__("mov %0, rbx\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_RSP(loc) \
-    __asm__("mov %0, rsp\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_RBP(loc) \
-    __asm__("mov %0, rbp\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-
-#define ASMTF_THREAD_STORE_RAX(loc) \
-    __asm__("mov %0, rax\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-
-#define ASMTF_THREAD_STORE_R8(loc) \
-    __asm__(                       \
-        "mov %0, r8\n\t"           \
-        :                          \
-        : "m"(loc)                 \
-        :)
-#define ASMTF_THREAD_STORE_R9(loc) \
-    __asm__(                       \
-        "mov %0, r9\n\t"           \
-        :                          \
-        : "m"(loc)                 \
-        :)
-
-#define ASMTF_THREAD_STORE_R10(loc) \
-    __asm__("mov %0, r10\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_R11(loc) \
-    __asm__("mov %0, r11\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_R12(loc) \
-    __asm__("mov %0, r12\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_R13(loc) \
-    __asm__("mov %0, r13\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_R14(loc) \
-    __asm__("mov %0, r14\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-#define ASMTF_THREAD_STORE_R15(loc) \
-    __asm__("mov %0, r15\n\t"       \
-            :                       \
-            : "m"(loc)              \
-            :)
-
-#define ASMTF_THREAD_LOAD_RDI(loc) \
-    __asm__("mov rdi, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RSI(loc) \
-    __asm__("mov rsi, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RDX(loc) \
-    __asm__("mov rdx, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RCX(loc) \
-    __asm__("mov rcx, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R8(loc) \
-    __asm__("mov r8, %0\n\t"      \
-            :                     \
-            : "m"(loc)            \
-            :)
-
-#define ASMTF_THREAD_LOAD_R9(loc) \
-    __asm__("mov r9, %0\n\t"      \
-            :                     \
-            : "m"(loc)            \
-            :)
-
-#define ASMTF_THREAD_LOAD_RBX(loc) \
-    __asm__("mov rbx, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RSP(loc) \
-    __asm__("mov rsp, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RBP(loc) \
-    __asm__("mov rbp, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_RAX(loc) \
-    __asm__("mov rax, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R10(loc) \
-    __asm__("mov r10, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R11(loc) \
-    __asm__("mov r11, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R12(loc) \
-    __asm__("mov r12, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R13(loc) \
-    __asm__("mov r13, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R14(loc) \
-    __asm__("mov r14, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_LOAD_R15(loc) \
-    __asm__("mov r15, %0\n\t"      \
-            :                      \
-            : "m"(loc)             \
-            :)
-
-#define ASMTF_THREAD_STORE_REGISTER0(loc) ASMTF_THREAD_STORE_RDI(loc)
-#define ASMTF_THREAD_STORE_REGISTER1(loc) ASMTF_THREAD_STORE_RSI(loc)
-#define ASMTF_THREAD_STORE_REGISTER2(loc) ASMTF_THREAD_STORE_RDX(loc)
-#define ASMTF_THREAD_STORE_REGISTER3(loc) ASMTF_THREAD_STORE_RCX(loc)
-#define ASMTF_THREAD_STORE_REGISTER4(loc) ASMTF_THREAD_STORE_R8(loc)
-#define ASMTF_THREAD_STORE_REGISTER5(loc) ASMTF_THREAD_STORE_R9(loc)
-#define ASMTF_THREAD_STORE_REGISTER6(loc) ASMTF_THREAD_STORE_RBX(loc)
-#define ASMTF_THREAD_STORE_REGISTER7(loc) ASMTF_THREAD_STORE_RSP(loc)
-#define ASMTF_THREAD_STORE_REGISTER8(loc) ASMTF_THREAD_STORE_RBP(loc)
-#define ASMTF_THREAD_STORE_REGISTER9(loc) ASMTF_THREAD_STORE_RAX(loc)
-#define ASMTF_THREAD_STORE_REGISTER10(loc) ASMTF_THREAD_STORE_R10(loc)
-#define ASMTF_THREAD_STORE_REGISTER11(loc) ASMTF_THREAD_STORE_R11(loc)
-#define ASMTF_THREAD_STORE_REGISTER12(loc) ASMTF_THREAD_STORE_R12(loc)
-#define ASMTF_THREAD_STORE_REGISTER13(loc) ASMTF_THREAD_STORE_R13(loc)
-#define ASMTF_THREAD_STORE_REGISTER14(loc) ASMTF_THREAD_STORE_R14(loc)
-#define ASMTF_THREAD_STORE_REGISTER15(loc) ASMTF_THREAD_STORE_R15(loc)
-
-#define ASMTF_THREAD_LOAD_REGISTER0(loc) ASMTF_THREAD_LOAD_RDI(loc)
-#define ASMTF_THREAD_LOAD_REGISTER1(loc) ASMTF_THREAD_LOAD_RSI(loc)
-#define ASMTF_THREAD_LOAD_REGISTER2(loc) ASMTF_THREAD_LOAD_RDX(loc)
-#define ASMTF_THREAD_LOAD_REGISTER3(loc) ASMTF_THREAD_LOAD_RCX(loc)
-#define ASMTF_THREAD_LOAD_REGISTER4(loc) ASMTF_THREAD_LOAD_R8(loc)
-#define ASMTF_THREAD_LOAD_REGISTER5(loc) ASMTF_THREAD_LOAD_R9(loc)
-#define ASMTF_THREAD_LOAD_REGISTER6(loc) ASMTF_THREAD_LOAD_RBX(loc)
-#define ASMTF_THREAD_LOAD_REGISTER7(loc) ASMTF_THREAD_LOAD_RSP(loc)
-#define ASMTF_THREAD_LOAD_REGISTER8(loc) ASMTF_THREAD_LOAD_RBP(loc)
-#define ASMTF_THREAD_LOAD_REGISTER9(loc) ASMTF_THREAD_LOAD_RAX(loc)
-#define ASMTF_THREAD_LOAD_REGISTER10(loc) ASMTF_THREAD_LOAD_R10(loc)
-#define ASMTF_THREAD_LOAD_REGISTER11(loc) ASMTF_THREAD_LOAD_R11(loc)
-#define ASMTF_THREAD_LOAD_REGISTER12(loc) ASMTF_THREAD_LOAD_R12(loc)
-#define ASMTF_THREAD_LOAD_REGISTER13(loc) ASMTF_THREAD_LOAD_R13(loc)
-#define ASMTF_THREAD_LOAD_REGISTER14(loc) ASMTF_THREAD_LOAD_R14(loc)
-#define ASMTF_THREAD_LOAD_REGISTER15(loc) ASMTF_THREAD_LOAD_R15(loc)
-
-#define ASMTF_THREAD_STORE_REGISTER(num, loc) ASMTF_THREAD_STORE_REGISTER##num(loc.index[num])
-#define ASMTF_THREAD_LOAD_REGISTER(num, loc) ASMTF_THREAD_LOAD_REGISTER##num(loc.index[num])
-
-#define ASMTF_THREAD_STORE_REGISTERS(loc) \
-    ASMTF_THREAD_STORE_REGISTER(0, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(1, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(2, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(3, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(4, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(5, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(6, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(7, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(8, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(9, loc);  \
-    ASMTF_THREAD_STORE_REGISTER(10, loc); \
-    ASMTF_THREAD_STORE_REGISTER(11, loc); \
-    ASMTF_THREAD_STORE_REGISTER(12, loc); \
-    ASMTF_THREAD_STORE_REGISTER(13, loc); \
-    ASMTF_THREAD_STORE_REGISTER(14, loc); \
-    ASMTF_THREAD_STORE_REGISTER(15, loc)
-
-#define ASMTF_THREAD_LOAD_REGISTERS(loc) \
-    ASMTF_THREAD_LOAD_REGISTER(0, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(1, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(2, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(3, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(4, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(5, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(6, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(7, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(8, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(9, loc);  \
-    ASMTF_THREAD_LOAD_REGISTER(10, loc); \
-    ASMTF_THREAD_LOAD_REGISTER(11, loc); \
-    ASMTF_THREAD_LOAD_REGISTER(12, loc); \
-    ASMTF_THREAD_LOAD_REGISTER(13, loc); \
-    ASMTF_THREAD_LOAD_REGISTER(14, loc); \
-    ASMTF_THREAD_LOAD_REGISTER(15, loc)
-
 enum asmtf_register_id {
     REGISTER_RDI = 0,
     REGISTER_RSI,
@@ -290,6 +30,234 @@ enum asmtf_register_id {
     REGISTER_R14,
     REGISTER_R15
 };
+
+#define ASMTF_THREAD_STORE_REGISTER_RDI(loc) \
+    __asm__(                        \
+        "mov %0, rdi\n\t"           \
+        :                           \
+        : "m"(loc)                  \
+        :)
+#define ASMTF_THREAD_STORE_REGISTER_RSI(loc) \
+    __asm__(                        \
+        "mov %0, rsi\n\t"           \
+        :                           \
+        : "m"(loc)                  \
+        :)
+#define ASMTF_THREAD_STORE_REGISTER_RDX(loc) \
+    __asm__(                        \
+        "mov %0, rdx\n\t"           \
+        :                           \
+        : "m"(loc)                  \
+        :)
+#define ASMTF_THREAD_STORE_REGISTER_RCX(loc) \
+    __asm__(                        \
+        "mov %0, rcx\n\t"           \
+        :                           \
+        : "m"(loc)                  \
+        :)
+
+#define ASMTF_THREAD_STORE_REGISTER_RBX(loc) \
+    __asm__("mov %0, rbx\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_RSP(loc) \
+    __asm__("mov %0, rsp\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_RBP(loc) \
+    __asm__("mov %0, rbp\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+
+#define ASMTF_THREAD_STORE_REGISTER_RAX(loc) \
+    __asm__("mov %0, rax\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+
+#define ASMTF_THREAD_STORE_REGISTER_R8(loc) \
+    __asm__(                       \
+        "mov %0, r8\n\t"           \
+        :                          \
+        : "m"(loc)                 \
+        :)
+#define ASMTF_THREAD_STORE_REGISTER_R9(loc) \
+    __asm__(                       \
+        "mov %0, r9\n\t"           \
+        :                          \
+        : "m"(loc)                 \
+        :)
+
+#define ASMTF_THREAD_STORE_REGISTER_R10(loc) \
+    __asm__("mov %0, r10\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_R11(loc) \
+    __asm__("mov %0, r11\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_R12(loc) \
+    __asm__("mov %0, r12\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_R13(loc) \
+    __asm__("mov %0, r13\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_R14(loc) \
+    __asm__("mov %0, r14\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+#define ASMTF_THREAD_STORE_REGISTER_R15(loc) \
+    __asm__("mov %0, r15\n\t"       \
+            :                       \
+            : "m"(loc)              \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RDI(loc) \
+    __asm__("mov rdi, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RSI(loc) \
+    __asm__("mov rsi, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RDX(loc) \
+    __asm__("mov rdx, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RCX(loc) \
+    __asm__("mov rcx, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R8(loc) \
+    __asm__("mov r8, %0\n\t"      \
+            :                     \
+            : "m"(loc)            \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R9(loc) \
+    __asm__("mov r9, %0\n\t"      \
+            :                     \
+            : "m"(loc)            \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RBX(loc) \
+    __asm__("mov rbx, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RSP(loc) \
+    __asm__("mov rsp, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RBP(loc) \
+    __asm__("mov rbp, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_RAX(loc) \
+    __asm__("mov rax, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R10(loc) \
+    __asm__("mov r10, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R11(loc) \
+    __asm__("mov r11, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R12(loc) \
+    __asm__("mov r12, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R13(loc) \
+    __asm__("mov r13, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R14(loc) \
+    __asm__("mov r14, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_LOAD_REGISTER_R15(loc) \
+    __asm__("mov r15, %0\n\t"      \
+            :                      \
+            : "m"(loc)             \
+            :)
+
+#define ASMTF_THREAD_STORE_REGISTER(register, loc) ASMTF_THREAD_STORE_##register(loc.index[(int)register])
+#define ASMTF_THREAD_LOAD_REGISTER(register, loc) ASMTF_THREAD_LOAD_##register(loc.index[(int)register])
+
+#define ASMTF_THREAD_STORE_REGISTERS(loc) \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RDI, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RSI, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RDX, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RCX, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R8, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R9, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RBX, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RSP, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RBP, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_RAX, loc);  \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R10, loc); \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R11, loc); \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R12, loc); \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R13, loc); \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R14, loc); \
+    ASMTF_THREAD_STORE_REGISTER(REGISTER_R15, loc)
+
+#define ASMTF_THREAD_LOAD_REGISTERS(loc) \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RDI, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RSI, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RDX, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RCX, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R8, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R9, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RBX, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RSP, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RBP, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_RAX, loc);  \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R10, loc); \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R11, loc); \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R12, loc); \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R13, loc); \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R14, loc); \
+    ASMTF_THREAD_LOAD_REGISTER(REGISTER_R15, loc)
+
+
 
 struct asmtf_sysv_call_t;
 
